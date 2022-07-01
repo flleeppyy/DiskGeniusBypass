0512fa08141bc841209ccd95d7e7fcc9de62ba5d
flleeppyy <flleeppyy@gmail.com>
2022-06-30 18:49:56 -0700

    Add project files.

---
+++ DiskGeniusBypass/LicenseFileHandler.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DiskGeniusBypass
+{
+    public class LicenseFileHandler
+    {
+        private string[] filePaths;
+        private StorageMethod storageMethod;
+        private List<MemStoreItem> memstore;
+        private List<FileStoreItem> filestore;
+        private string tempFolder;
+        public enum StorageMethod
+        {
+            Memory = 0,
+            MoveFiles = 1,
+        }
+
+        public LicenseFileHandler(StorageMethod storageMethod, string[] inputFilePaths)
+        {
+            Console.WriteLine(String.Join(Environment.NewLine, inputFilePaths));
+
+            this.storageMethod = storageMethod;
+            this.filePaths = inputFilePaths;
+            if (storageMethod == StorageMethod.Memory)
+            {
+                memstore = new List<MemStoreItem>();
+                for (int i = 0; i < inputFilePaths.Length; i++)
+                {
+                    memstore.Add(new MemStoreItem(Path.GetFileName(inputFilePaths[i]), inputFilePaths[i]));
+                }
+            } else if (storageMethod == StorageMethod.MoveFiles)
+            {
+                filestore = new List<FileStoreItem>();
+                tempFolder = "temp" + Guid.NewGuid().ToString().Split(char.Parse("-"))[0];
+                for (int i = 0; i < inputFilePaths.Length; i++)
+                {
+                    filestore.Add(new FileStoreItem(Path.GetFileName(inputFilePaths[i]), inputFilePaths[i]));
+                }
+            }
+        }
+
+        public void Store()
+        {
+            if (storageMethod == StorageMethod.Memory)
+            {
+                for (int i = 0; i < memstore.Count; i++)
+                {
+                    memstore[i].Data = File.ReadAllBytes(memstore[i].Filepath);
+                    File.Delete(memstore[i].Filepath);
+                }
+            } else if (storageMethod == StorageMethod.MoveFiles)
+            {
+                Console.WriteLine("move files method");
+                if (!File.Exists(tempFolder))
+                {
+                    Directory.CreateDirectory(tempFolder);
+                }
+
+                Console.WriteLine(filePaths.Length);
+                for (int i = 0; i < filePaths.Length; i++)
+                {
+                    Console.WriteLine("Storing file {0}", filestore[i].Filename);
+                    string newPath = Path.Combine(tempFolder, filestore[i].Filename);
+                    File.Move(filestore[i].OriginalFilepath, newPath);
+                    filestore[i].CurrentFilepath = newPath;
+                }
+            }
+        }
+
+        public void Restore()
+        {
+            if (storageMethod == StorageMethod.Memory)
+            {
+                for (int i = 0; i < memstore.Count; i++)
+                {
+                    File.WriteAllBytes(memstore[i].Filepath, memstore[i].Data);
+                }
+            }
+            else if (storageMethod == StorageMethod.MoveFiles)
+            {
+                for (int i = 0; i < filestore.Count; i++)
+                {
+                    Console.WriteLine("Restoring file {0}", filestore[i].Filename);
+                    Console.WriteLine(filestore[i].CurrentFilepath);
+                    File.Move(filestore[i].CurrentFilepath, filestore[i].OriginalFilepath);
+                    filestore[i].CurrentFilepath = filestore[i].OriginalFilepath;
+                }
+                Directory.Delete(tempFolder, true);
+            }
+        }
+
+        private class MemStoreItem
+        {
+            public string Filename;
+            public string Filepath;
+            public byte[] Data;
+
+            public MemStoreItem(string name, string path)
+            {
+                Filename = name;
+                Filepath = path;
+                Data = File.ReadAllBytes(Filepath);
+            }
+        }
+
+        private class FileStoreItem
+        {
+            public string Filename;
+            public string OriginalFilepath;
+            public string CurrentFilepath;
+
+            public FileStoreItem(string name, string path)
+            {
+                Filename = name;
+                OriginalFilepath = CurrentFilepath = path;
+            }
+
+        }
+    }
+
+}
---
+++ DiskGeniusBypass/Main.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Security.Principal;
+using static DiskGeniusBypass.LicenseFileHandler;
+
+namespace DiskGeniusBypass
+{
+    class DiskGeniusBypass
+    {
+        private static string DiskGeniusPath = Directory.GetCurrentDirectory();
+        private static string[] LicenseFiles =
+        {
+            "msimg32.dll",
+            "OfflineReg.exe",
+            "Options.ini"
+        };
+
+        private static Process DGProcess;
+
+        private static bool DGexited = false;
+
+        private static string DiskGeniusExecutable = "DiskGenius_.exe";
+
+        // Memory or MoveFiles
+        private static StorageMethod StoreType = StorageMethod.Memory;
+
+        public static void Main(string[] args)
+        {
+            // Check if we're an Administrator
+            if (!IsAdministrator())
+            {
+                MessageBox.Show("The program is not running as administrator. Please run as administrator", "Failed to start", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            formatLicensePaths();
+
+            Bypass();
+        }
+
+        public static void formatLicensePaths()
+        {
+            for (int i = 0; i < LicenseFiles.Length; i++)
+            {
+                LicenseFiles[i] = Path.Combine(DiskGeniusPath,LicenseFiles[i]);
+            }
+        }
+        public static void Bypass()
+        {
+            string path = Path.Combine(DiskGeniusPath, DiskGeniusExecutable);
+            if (File.Exists(path))
+            {
+
+                // Check if there's a previous process and kill it/dispose of it.
+                if (DGProcess != null)
+                {
+                    //(Process DGProcess.HasExited) ? DGProcess.Kill() : DGProcess.Dispose();
+                    if (!DGProcess.HasExited) 
+                        DGProcess.Kill();
+                    DGProcess.Dispose();
+                    DGexited = false;
+                }
+
+                // Create the process
+                DGProcess = new Process
+                {
+                    StartInfo = new ProcessStartInfo(path),
+                    EnableRaisingEvents = true
+                };
+                DGProcess.Exited += new EventHandler(DiskGenius_Exited);
+
+                // Store the files
+                LicenseFileHandler fileHandler = new LicenseFileHandler(LicenseFileHandler.StorageMethod.Memory, LicenseFiles);
+                fileHandler.Store();
+
+                // Start it
+                DGProcess.Start();
+
+                Thread.Sleep(3000);
+                if (DGexited)
+                {
+                    MessageBox.Show("The bypass most likely failed since DiskGenius exited early.", "Bypass failed", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                }
+                fileHandler.Restore();
+            }
+            else
+            {
+                MessageBox.Show("DiskGenius_.exe could not be found. Did you forget to rename it? Are you in the right folder?", "Warning");
+            }
+        }
+
+        // Handle Exited event and display process information.
+        private static void DiskGenius_Exited(object sender, EventArgs e)
+        {
+            
+        }
+
+        // http://stackoverflow.com/questions/11660184/ddg#11660205
+        public static bool IsAdministrator()
+        {
+            return (new WindowsPrincipal(WindowsIdentity.GetCurrent()))
+                      .IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        // This method stores the license files in 
+        
+    }
+}
